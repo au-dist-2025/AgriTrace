d992fa182e7a2c006f39350ff5c8b2312e0dda48
Saravana-kumar-j <saravanakumarj.net@gmail.com>
2025-04-14 02:20:41 +0530

    Remove .next from git tracking

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/sarky/Dev/fyp-1/d-mandates-main/app/layout\":[\"static/media/eafabf029ad39a43-s.p.woff2\",\"static/media/8888a3826f4a3af4-s.p.woff2\",\"static/media/0484562807a97172-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
